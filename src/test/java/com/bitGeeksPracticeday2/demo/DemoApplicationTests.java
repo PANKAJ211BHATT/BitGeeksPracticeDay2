17080e2b690b3dee77609ce225397c1467b20a1f
PANKAJ BHATT <211pankajbhatt@gmail.com>
2025-09-24 13:54:41 +0530

    another commit

---
+++ src/test/java/com/bitGeeksPracticeday2/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitGeeksPracticeday2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
